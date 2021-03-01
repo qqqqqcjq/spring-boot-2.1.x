238ffa349a961d292d859e6cc2360ad53b29dfd0
chengjiaqing <caverspark@163.com>
2021-03-01 13:32:52 +0800

    注释不用的gradle支持类

--- spring-boot-project/spring-boot-tools/spring-boot-gradle-plugin/src/test/java/org/springframework/boot/gradle/plugin/DependencyManagementPluginActionIntegrationTests.java
+++ spring-boot-project/spring-boot-tools/spring-boot-gradle-plugin/src/test/java/org/springframework/boot/gradle/plugin/DependencyManagementPluginActionIntegrationTests.java
@@ -19,8 +19,8 @@ package org.springframework.boot.gradle.plugin;
 import java.io.File;
 import java.io.IOException;
 
-import org.gradle.testkit.runner.BuildResult;
-import org.gradle.testkit.runner.TaskOutcome;
+//import org.gradle.testkit.runner.BuildResult;
+//import org.gradle.testkit.runner.TaskOutcome;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -37,37 +37,38 @@ import static org.assertj.core.api.Assertions.assertThat;
  *
  * @author Andy Wilkinson
  */
+//支持gradle的一个组件，用不到，而且没有用gradle,也没有引入gradle相关jar包，这个类会报错，暂时全部注释掉
 @RunWith(GradleCompatibilitySuite.class)
 public class DependencyManagementPluginActionIntegrationTests {
 
-	@Rule
-	public GradleBuild gradleBuild;
-
-	@Test
-	public void noDependencyManagementIsAppliedByDefault() {
-		assertThat(this.gradleBuild.build("doesNotHaveDependencyManagement").task(":doesNotHaveDependencyManagement")
-				.getOutcome()).isEqualTo(TaskOutcome.SUCCESS);
-	}
-
-	@Test
-	public void bomIsImportedWhenDependencyManagementPluginIsApplied() {
-		assertThat(this.gradleBuild.build("hasDependencyManagement", "-PapplyDependencyManagementPlugin")
-				.task(":hasDependencyManagement").getOutcome()).isEqualTo(TaskOutcome.SUCCESS);
-	}
-
-	@Test
-	public void helpfulErrorWhenVersionlessDependencyFailsToResolve() throws IOException {
-		File examplePackage = new File(this.gradleBuild.getProjectDir(), "src/main/java/com/example");
-		examplePackage.mkdirs();
-		FileSystemUtils.copyRecursively(new File("src/test/java/com/example"), examplePackage);
-		BuildResult result = this.gradleBuild.buildAndFail("compileJava");
-		assertThat(result.task(":compileJava").getOutcome()).isEqualTo(TaskOutcome.FAILED);
-		String output = result.getOutput();
-		assertThat(output).contains("During the build, one or more dependencies that "
-				+ "were declared without a version failed to resolve:");
-		assertThat(output).contains("org.springframework.boot:spring-boot-starter-web:");
-		assertThat(output).contains("Did you forget to apply the " + "io.spring.dependency-management plugin to the "
-				+ this.gradleBuild.getProjectDir().getName() + " project?");
-	}
+//	@Rule
+//	public GradleBuild gradleBuild;
+//
+//	@Test
+//	public void noDependencyManagementIsAppliedByDefault() {
+//		assertThat(this.gradleBuild.build("doesNotHaveDependencyManagement").task(":doesNotHaveDependencyManagement")
+//				.getOutcome()).isEqualTo(TaskOutcome.SUCCESS);
+//	}
+//
+//	@Test
+//	public void bomIsImportedWhenDependencyManagementPluginIsApplied() {
+//		assertThat(this.gradleBuild.build("hasDependencyManagement", "-PapplyDependencyManagementPlugin")
+//				.task(":hasDependencyManagement").getOutcome()).isEqualTo(TaskOutcome.SUCCESS);
+//	}
+//
+//	@Test
+//	public void helpfulErrorWhenVersionlessDependencyFailsToResolve() throws IOException {
+//		File examplePackage = new File(this.gradleBuild.getProjectDir(), "src/main/java/com/example");
+//		examplePackage.mkdirs();
+//		FileSystemUtils.copyRecursively(new File("src/test/java/com/example"), examplePackage);
+//		BuildResult result = this.gradleBuild.buildAndFail("compileJava");
+//		assertThat(result.task(":compileJava").getOutcome()).isEqualTo(TaskOutcome.FAILED);
+//		String output = result.getOutput();
+//		assertThat(output).contains("During the build, one or more dependencies that "
+//				+ "were declared without a version failed to resolve:");
+//		assertThat(output).contains("org.springframework.boot:spring-boot-starter-web:");
+//		assertThat(output).contains("Did you forget to apply the " + "io.spring.dependency-management plugin to the "
+//				+ this.gradleBuild.getProjectDir().getName() + " project?");
+//	}
 
 }
